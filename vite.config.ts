1beeb814f79ce9abd9fbd19457837c1b620d6d51
miguelggdev <magonzalezg@udistrital.edu.co>
2026-07-25 19:39:28 -0500

    fix: base path correcto para repo speedskatetrack

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/speedskatetrack-landing/' : '/',
+  base: mode === 'production' ? '/speedskatetrack/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
